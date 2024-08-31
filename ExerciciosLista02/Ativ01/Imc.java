633fd33a4889ab23dcdaa9ee3070061f8ae2fe5b
PabloMelo2005 <pablobertoldo.melo@gmail.com>
2024-08-30 22:14:33 -0300

    exercicios lista 2 sistemas distribuidos

---
+++ ExerciciosLista02/Ativ01/Imc.java
@@ -0,0 +1,9 @@
+
+public class Imc {
+
+    public double calculoImc(double peso, double altura) {
+
+        double imc = peso / (Math.pow(altura, 2));
+        return imc;
+    }
+}
---
+++ ExerciciosLista02/Ativ03/Acoes.java
@@ -0,0 +1,11 @@
+public interface Acoes {
+
+    void acordar();
+
+    void andar();
+
+    void comer();
+
+    void dormir();
+
+}
